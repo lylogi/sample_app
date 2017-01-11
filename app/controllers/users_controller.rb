246d38c61ee9d1e466a1cbf05151fbdd113f55b7
lylogi <ngly58@gmail.com>
2017-01-12 02:36:11 +0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
-  
+  before_action :logged_in_user, except: [:show, :new, :create]
+  before_action :find_user, except: [:index, :new, :create]
+  before_action :admin_user, only: :destroy
+  before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.paginate page: params[:page]
+  end
+
   def show
-    @user = User.find_by id: params[:id]
     if @user.nil?
       flash[:error] = "Find not found!"
       redirect_to root_path
@@ -19,14 +26,57 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
-      render "new"
+      render :new
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = "User deleted"
+    redirect_to root_url
   end
 
   private 
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    unless @user
+      flash[:warning] = "Couldn't find this user. Please try again!"
+      redirect_to root_url
+    end
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    find_user
+    redirect_to root_url unless current_user.current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 
 end
